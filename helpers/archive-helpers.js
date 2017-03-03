91a52d689ed1aae8251dcbb97fbc7e1032232c92
kevinhuangg <email.kevinh@gmail.com>
2017-03-02 16:46:14 -0800

    still working on helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -14,7 +14,8 @@ exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
   archivedSites: path.join(__dirname, '../archives/sites'),
   list: path.join(__dirname, '../archives/sites.txt'),
-  index: path.join(__dirname, '../web/public/index.html')
+  index: path.join(__dirname, '../web/public/index.html'),
+  loading: path.join(__dirname, '../web/public/loading.html')
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -28,14 +29,38 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
-//listen for when fs is finished getting archive/sites.txt
-  //execute callback
+  fs.readFile(exports.paths.list, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var array = data.toString().split('\n');
+    callback(array);
+  }); 
+  //return array of urls
 };
 
 exports.isUrlInList = function(url, callback) {
+  readListOfUrls(function(urlArray) {
+    console.log(urlArray);
+    urlArray.forEach(function(urlElement) {
+      console.log(urlElement, "ELEMENt");
+      if (urlElement === url) {
+        callback(true);
+        return;
+      }
+    });
+    callback(false);
+  });
+  //callback we want to check if url === iterate through array
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, url + '\n', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    callback(); 
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -5,63 +5,146 @@ var fs = require('fs');
 var headers = require('./http-helpers.js').headers; //might need it?
 var serveAssets = require('./http-helpers.js').serveAssets;
 
-// require more modules/folders here!
 // http://127.0.0.1:8080/
 exports.handleRequest = function (req, res) {
   // res.end(archive.paths.list);
 
   var statusCode;
-  var body = [];
+
+  //declare variable to hold index.html data
+  var index = fs.readFileSync(archive.paths.index);
+  //declare variable to hold loading.html data
+  var loading = fs.readFileSync(archive.paths.loading);
   
+  // fs.readFile(archive.paths.index, function(err, data) {
+  //   if (err) {
+  //     statusCode = 404; 
+  //   }
+  //   index = data;
+  // }).readFile(archive.paths.loading, function(err, data) {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   loading = data;
+  // });
+
   req.on('error', function() {
     statusCode = 404;
   });
-
-  if (req.url === '/') {
+  
+  //GET method
+  if (req.method === 'GET') { 
+    
     statusCode = 200; 
-    fs.readFile(archive.paths.index, function(err, data) {
-      if (err) {
-        throw err;
-      }
+    
+    if (req.url === '/') { //if going to homepage
+
       res.writeHead(statusCode, headers); 
-      res.end(data);
+      res.end(index);
 
-    });
-  } else {
-    statusCode = 200; 
+    } else { //if going directly to archived site
    
-    fs.readFile(archive.paths.archivedSites + req.url, function(err, data) {
-      if (err) {
-        statusCode = 404;
-      }
-      res.writeHead(statusCode, headers); 
-      res.end(data);
+      fs.readFile(archive.paths.archivedSites + req.url, function(err, data) {
+        if (err) {
+          statusCode = 404;
+        }
+        res.writeHead(statusCode, headers); 
+        res.end(data);
+      });  
 
-    });
+    }
   }
 
   if (req.method === 'POST') {
-    req.on('data', function(chunk) {
-      statusCode = 201; 
-      // console.log(chunk);
-      var jsonString = '';
-      jsonString += chunk; //output: url=www.example.com
-      // console.log(jsonString, 'JSONSTRING');
-    }).on('end', function() {
-      //invoke readListOfUrls
-        //if (!isUrlInList)
-          //invoke addUrlToList
+    statusCode = 201;
+    var url = '';
+    //receive data convert to string and add to archives if its not in there
+    req.on('error', function(err) {
+      console.log(err);
+    }).on('data', function(data) {
+      url += data; //url=www.example.com
+      fs.readFile(archive.paths.list, function(err, data) {
+        if (err) {
+          throw err;
+        }
+        var array = data.toString().split('\n'); 
+
+        if (array.indexOf(url.slice(4)) === -1) { //if we can't find it
+          statusCode = 302;
+
+          fs.appendFile(archive.paths.list, url.slice(4) + '\n', function(err, data) {
+            if (err) {
+              throw err;
+            }
+            console.log('The url was appended to file!');
+          });
+          
+          res.writeHead(statusCode, headers); 
+          res.end(loading);
+        }
+      }); 
     });
-      
-      //store jsonString inside of archives/sites/sites.txt
-
-      //assuming we have the correct output
-      //check if the output is in archives/sites/sites.txt
-
+  //   res.writeHead(statusCode, headers);
+  //   res.end();
+  // }
   }
 
-  //get url from body of request
-    //if url is in sites/sites.txt 
-      //send back sites/url.js
-  // res.writeHead(statusCode, headers); 
+
+
 };
+
+
+
+
+
+
+
+
+  // if (req.url === '/') {
+  //   statusCode = 200; 
+  //   // fs.readFile(archive.paths.index, function(err, data) {
+  //   //   if (err) {
+  //   //     throw err;
+  //   //   }
+
+  //   res.writeHead(statusCode, headers); 
+  //   res.end(index);
+
+  // } else if (req.method === 'GET'){ 
+  //   statusCode = 200; 
+   
+  //   fs.readFile(archive.paths.archivedSites + req.url, function(err, data) {
+  //     if (err) {
+  //       statusCode = 404;
+  //     }
+  //     res.writeHead(statusCode, headers); 
+  //     res.end(data)
+  //   }); 
+
+  // } else {
+  //   fs.readFile(archive.paths.list, function(err, data) {
+  //     if (err) {
+  //       throw err;
+  //     }
+  //     var array = data.toString().split('\n'); 
+  //     // console.log(array);
+  //     // console.log(req.url);
+  //     if (array.indexOf(req.url.slice(5)) === -1) {
+  //       //append url to sites.txt
+  //       // console.log('yup');
+  //       fs.appendFile(archive.paths.list, req.url.slice(5), 'a', function(err, data) {
+  //         if (err) {
+  //           throw err;
+  //         }
+  //         console.log('The url was appended to file!');
+  //       });
+  //       //redirect to loading.html
+  //       res.end(loading);
+  //     } else {
+  //       //redirect to 
+  //     }
+  //   }
+  // }
+      //if url is not in list
+        //write url into sites.txt
+
